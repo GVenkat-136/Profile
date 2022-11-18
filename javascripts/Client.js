568d2820a27db26b2dc6300d003af1d552560bfe
GVENKATANARENDRA <103510399+GVenkat-136@users.noreply.github.com>
2022-11-18 09:51:02 +0530

    first commit

--- javascripts/Client.js
+++ javascripts/Client.js
@@ -6,6 +6,14 @@ const sendmassage = () => {
   message.phone = $("#Phone").val();
   message.subject = $("#Subject").find(":selected").text();
   message.regarding = $("textarea#message").val();
+
+  Email.send({
+    SecureToken: "8faf388a-8aac-4e12-8171-dc4942e38dfa",
+    To: "dev.gvnarendra@gmail.com",
+    From: "dev.gvnarendra@gmail.com",
+    Subject: message.subject,
+    Body: `This ${message.mail} This My Requirement ${message.regarding}`,
+  }).then((message) => alert(message));
   clear();
 };
 const clear = () => {
@@ -15,3 +23,21 @@ const clear = () => {
   message.subject = $("#Subject").find(":selected").text("");
   message.regarding = $("#message").val("");
 };
+
+const sendmail = () => {
+  const message1 = {};
+  message1.mail1 = $("#usermailid").val();
+  Email.send({
+    SecureToken: "8faf388a-8aac-4e12-8171-dc4942e38dfa",
+    To: "dev.gvnarendra@gmail.com",
+    From: "dev.gvnarendra@gmail.com",
+    Subject:
+      "one User Is Send The Her/Him mail Id For talk To you Send the your Responce to him/her",
+    Body: `This ${message1.mail1}  my Mail Id I need talk to you I'm wait for your respones`,
+  }).then((message) => alert(message));
+  clearsendmail();
+};
+const clearsendmail = () => {
+  message1.mail1 = $("#usermailid").val("");
+};
+// -----------------   Send Mail Loging  Logics ----------------//
